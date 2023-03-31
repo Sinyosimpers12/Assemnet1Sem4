6ebbf32c8506207bcb38eae06c1bf8b79e128be7
Sinyo Simpers Soba <inyorinyo@outlook.com>
2023-03-31 18:35:36 +0700

    Initial commit 2

--- app/src/main/java/org/d3if3075/assesment_sinyo/MainActivity.kt
+++ app/src/main/java/org/d3if3075/assesment_sinyo/MainActivity.kt
@@ -6,6 +6,11 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener { Hitung() }
+    }
+    private fun Hitung() {
+        Log.d("MainActivity", "Tombol diklik!")
     }
 }
