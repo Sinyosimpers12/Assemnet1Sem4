709881d34bc321f0f4b81df00f32aaa01c5933e0
MuhammadHudaibi <muhhudaibi11@gmail.com>
2023-03-31 22:23:55 +0700

    Fix

--- app/src/main/java/org/d3if3075/assesment_sinyo/MainActivity.kt
+++ app/src/main/java/org/d3if3075/assesment_sinyo/MainActivity.kt
@@ -2,15 +2,38 @@ package org.d3if3075.assesment_sinyo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import org.d3if3075.assesment_sinyo.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button.setOnClickListener { Hitung() }
+
+        binding.x.setOnClickListener { hitungluas() }
+        }
+
+    private fun hitungluas() {
+        val lebar = binding.lebar.text.toString()
+        if (TextUtils.isEmpty(lebar)) {
+            Toast.makeText(this, R.string.lebarIn, Toast.LENGTH_LONG).show()
+            return
+        }
+        val tinggi = binding.tinggi.text.toString()
+        if (TextUtils.isEmpty(tinggi)) {
+            Toast.makeText(this, R.string.tinggi_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val hasil = lebar.toFloat()*tinggi.toFloat()
+
+            binding.textView4.text = hasil.toString()+" cm[2]"
+
     }
-    private fun Hitung() {
-        Log.d("MainActivity", "Tombol diklik!")
     }
-}
+
